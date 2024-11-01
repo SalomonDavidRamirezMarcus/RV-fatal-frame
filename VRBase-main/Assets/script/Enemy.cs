2812bf308420b3bea3b1fbfb6a23725755199a96
andresF864 <159442816+andresF864@users.noreply.github.com>
2024-11-01 00:51:12 -0500

    Update Enemy.cs

--- VRBase-main/Assets/script/Enemy.cs
+++ VRBase-main/Assets/script/Enemy.cs
@@ -6,12 +6,12 @@ public class Enemy : MonoBehaviour
 {
     public int maxHealth = 100;
     private int currentHealth;
-    private Animator anim;
+    //private Animator anim;
 
     void Start()
     {
         currentHealth = maxHealth;
-        anim = GetComponent<Animator>();
+        //anim = GetComponent<Animator>();
     }
 
     public void TakeDamage(int damage)
